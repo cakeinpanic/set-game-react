9e60d726d9d946eac5fa638b4e6b1cdee22dcae6
cakeinpanic <cakeinpanic@users.noreply.github.com>
2020-07-14 13:29:35 +0000

    deploy: 084ad5319b490fd2bd469d5ab3bcde5ec6f48757

---
+++ precache-manifest.cb7d0892644eb9840baae5f31bcdda60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2bd65c728b69ca4a0d689de91ac9ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69d99206c3360c69e92e",
+    "url": "/static/css/main.bf2d05bf.chunk.css"
+  },
+  {
+    "revision": "1927c21372e5fd0376ce",
+    "url": "/static/js/2.8acb72e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8acb72e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d99206c3360c69e92e",
+    "url": "/static/js/main.ce4ddfd4.chunk.js"
+  },
+  {
+    "revision": "09f59d9586b795995172",
+    "url": "/static/js/runtime-main.1875ab06.js"
+  }
+]);
